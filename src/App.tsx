38f352421906180cf16d4ca209e11e4b24e84931
André Matheus Fil <andrematheusfil@gmail.com>
2026-07-06 21:06:36 -0300

    Feat: Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { View } from "react-native";
 import { Home } from "./pages/Home";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
 
 export function App() {
+  const [loaded, error] = useFonts({
+    InterRegular: Inter_400Regular,
+    InterBold: Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Home/>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Home/>
+    </View>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export const Home = () => {
   return (
     <View>
-        <Text>Home</Text>
+        <Text style={{ fontFamily: "InterBold", fontSize: 50 }}>Home</Text>
     </View>
   );
 }
